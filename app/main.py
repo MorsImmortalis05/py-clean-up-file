c527444c02168af9d54a95cc66a69eb1c904fe0c
Liliya Kyrylyshena <morsimmortalis05@gmail.com>
2025-07-09 22:03:47 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,28 @@
+import os
+from typing import Any
+from types import TracebackType
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+
+    def __enter__(self) -> Any:
+        try:
+            self.file = open(self.filename, "r")
+            return self.file
+        except FileNotFoundError as e:
+            print(f"didko, something went wrong! {e}")
+        return self
+
+    def __exit__(
+            self,
+            exc_type: type[BaseException],
+            exc_val: BaseException,
+            exc_tb: TracebackType,) -> Any:
+        if exc_type:
+            if issubclass(exc_type, FileNotFoundError):
+                print(f"didko, something went wrong! {exc_val}")
+                return True
+        if os.path.exists(f"{self.filename}"):
+            os.remove(self.filename)
